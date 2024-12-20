63661fbb58ae7815fd539b91742ac27a51fd0bbe
Erik Fogelstrand <erikfog@chalmers.se>
2024-12-20 12:44:13 +0100

    man kan äta

--- Animal farm/src/Model/Player/IMovementHandler.java
+++ Animal farm/src/Model/Player/IMovementHandler.java
@@ -5,6 +5,7 @@ import java.awt.*;
 public interface IMovementHandler {
     public void move(int x, int y );
     public Point getPos();
-    public void interact(int x, int y);
+    public void tileInteract(int x, int y);
+    public void playerInteract();
     public void setDirection(int x, int y);
 }
